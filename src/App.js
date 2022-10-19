d37a956d990833ed812fe88c02b47def42ecfe52
Adarsh Pandey <mail.apandey2001@gmail.com>
2022-10-19 20:45:36 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import Mainarea from './Components/Mainarea';
+import Navbar from './Components/Navbar';
+import {useState} from 'react';
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  if (theme === "light") {
+    document.body.style.backgroundColor = "#fff";
+  } else {
+    document.body.style.backgroundColor = "#1c1c1c";
+  }
+
+  const handleSwitchTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+
   return (
-    <h1>Weather App</h1>
+    <>
+      <Navbar theme={theme} handleSwitchTheme={handleSwitchTheme} />
+      <Mainarea theme={theme} />
+    </>
   );
 }
 
