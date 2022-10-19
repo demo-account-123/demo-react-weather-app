bf1fbbb02457bc2541c3af1e459068295808fc90
Adarsh Pandey <mail.apandey2001@gmail.com>
2022-10-19 19:30:15 +0530

    File setup and cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import './App.css';
-import Mainarea from './Components/Mainarea';
-import Navbar from './Components/Navbar';
-import {useState} from 'react';
 
 function App() {
-  const [theme, setTheme] = useState("light");
-
-  if (theme === "light") {
-    document.body.style.backgroundColor = "#fff";
-  } else {
-    document.body.style.backgroundColor = "#1c1c1c";
-  }
-
-  const handleSwitchTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  }
-
   return (
-    <>
-      <Navbar theme={theme} handleSwitchTheme={handleSwitchTheme} />
-      <Mainarea theme={theme} />
-    </>
+    <h1>Weather App</h1>
   );
 }
 
